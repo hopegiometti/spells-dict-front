b5f01c664da6ab4f6ddb52c8313eb363397f8804
Hope <hopegiometti@gmail.com>
2020-10-05 10:25:42 -0600

    added styling to browse

--- src/EachSpell.js
+++ src/EachSpell.js
@@ -5,6 +5,9 @@ export const StyledDiv = styled.div`
     padding: 25px;
     text-align: center;
     font-family: Lumos;
+    width: 250px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    padding-bottom: 15px;
 `
 
 export const StyledText = styled.h5`
@@ -13,8 +16,12 @@ export const StyledText = styled.h5`
 
 export const EachSpell = (props) => {
     return( <div>
-        <StyledDiv>{props.spellName}</StyledDiv>
-        {/* {console.log(props)}   */}
+        <StyledDiv>
+            <h1>{props.spellName}</h1>
+            <h2>Effect: {props.effect}</h2>
+            <h3>Type: {props.type}</h3>
+        </StyledDiv>
+        {console.log(props)}  
     </div> )
 }
 
