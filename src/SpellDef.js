7802b3bad2fcad2fadbccfd147e31597e968e452
Hope <hopegiometti@gmail.com>
2020-10-05 10:22:26 -0600

    updated search style

--- src/SpellDef.js
+++ src/SpellDef.js
@@ -4,6 +4,9 @@ import styled from 'styled-components'
 export const StyledDiv = styled.div`
     text-align: center;
     font-family: Lumos;
+    width: 250px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    padding-bottom: 15px;
 `
 
 export const SpellDef = (props) => {
